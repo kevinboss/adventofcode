2a0c01384837b8d12613de5be0f2237663c7081c
Kevin Boss <kevin.boss@outlook.com>
2024-12-06 23:10:05 +0100

    day 2

---
+++ 2/2_2.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	file, _ := os.Open("2/input.txt")
+	scanner := bufio.NewScanner(file)
+	var safeLevels = 0
+	for scanner.Scan() {
+		parts := strings.Fields(scanner.Text())
+		var numbers []int
+		for _, part := range parts {
+			num, _ := strconv.Atoi(part)
+			numbers = append(numbers, num)
+		}
+		var safeLevel = isLevelSafe(numbers)
+		for i := 0; i < len(numbers); i++ {
+			variation := make([]int, len(numbers))
+			copy(variation, numbers)
+			safeLevelVariation := isLevelSafe(append(variation[:i], variation[i+1:]...))
+			if safeLevelVariation {
+				safeLevel = true
+			}
+		}
+
+		if safeLevel {
+			safeLevels++
+		}
+	}
+	fmt.Println(safeLevels)
+}
+
+func isLevelSafe(numbers []int) bool {
+	var safeLevel = true
+	for i := 0; i < len(numbers)-1; i++ {
+		left := numbers[i]
+		right := numbers[i+1]
+		diff := left - right
+		var absDiff int
+		if diff > 0 {
+			absDiff = diff
+		} else {
+			absDiff = -diff
+		}
+		if absDiff < 1 || absDiff > 3 {
+			safeLevel = false
+		}
+	}
+	for i := 1; i < len(numbers)-1; i++ {
+		left := numbers[i-1]
+		middle := numbers[i]
+		right := numbers[i+1]
+		if (left < middle && middle > right) || (left > middle && middle < right) {
+			safeLevel = false
+		}
+	}
+	return safeLevel
+}
